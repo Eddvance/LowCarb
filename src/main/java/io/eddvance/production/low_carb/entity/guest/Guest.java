2e06591e71f510f573fa08a42eb0eead98637014
Edouard Leroy <leroyedouard@protonmail.com>
2025-05-06 18:53:15 +0200

    work on LowCarb Entity Guest

---
+++ src/main/java/io/eddvance/production/low_carb/entity/guest/Guest.java
@@ -0,0 +1,44 @@
+package io.eddvance.production.low_carb.entity.guest;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Guest {
+
+    @Id
+    private Long id;
+    private String email;
+    private String ratingRequest;
+
+    public Guest(String ratingRequest, String email, Long id) {
+        this.ratingRequest = ratingRequest;
+        this.email = email;
+        this.id = id;
+    }
+    public Guest() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRatingRequest() {
+        return ratingRequest;
+    }
+
+    public void setRatingRequest(String ratingRequest) {
+        this.ratingRequest = ratingRequest;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
