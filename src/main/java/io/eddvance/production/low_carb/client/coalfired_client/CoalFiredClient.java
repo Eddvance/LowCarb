0b0ed3e97c89ddce2525d6b6e4ca6e2fb50dcf80
Edouard Leroy <leroyedouard@protonmail.com>
2025-06-02 22:21:54 +0200

    MEP eureka et config Feign en cours 2.1

--- src/main/java/io/eddvance/production/low_carb/client/coalfired_client/CoalFiredClient.java
+++ src/main/java/io/eddvance/production/low_carb/client/coalfired_client/CoalFiredClient.java
@@ -3,8 +3,8 @@ package io.eddvance.production.low_carb.client.coalfired_client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="coalfired")
+@FeignClient(name="coal-fired")
 public interface CoalFiredClient {
-    @GetMapping("/coalfired")
+    @GetMapping("/coal-fired")
     String getCarbonEnergyRate();//
 }
