cc6b1d2caa58df64cdbc56fc06f16781b5e5a811
Edouard Leroy <leroyedouard@protonmail.com>
2025-06-12 20:02:09 +0200

    ServiceCron = Suppression de Feign pour WebClient (erreur) - creation de RateHistoryRepository, RateHistoryService, WebClientConfig, RateScheduler, CronController.  maj entity RateRecord.

--- src/main/java/io/eddvance/production/lowcarb/LowCarbApplication.java
+++ src/main/java/io/eddvance/production/lowcarb/LowCarbApplication.java
@@ -3,11 +3,9 @@ package io.eddvance.production.lowcarb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class LowCarbApplication {
 
     public static void main(String[] args) {
